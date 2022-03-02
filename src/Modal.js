2dd28a93efe7e729f574936f8503d4234c96dc51
Jeanie Ahn <Jeanie.ahn9@gmail.com>
2022-03-02 22:32:49 +0100

    Portals

---
+++ src/Modal.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+    return () => modalRoot.removeChild(elRef.current); 
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current); //for cleanups, like stopping timers, removing things from the DOM to prevent memory leak
+};
+
+export default Modal;
