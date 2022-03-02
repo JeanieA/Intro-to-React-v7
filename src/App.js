be36efc7513eefca21791bd2fd3b467d01c120b6
Jeanie Ahn <Jeanie.ahn9@gmail.com>
2022-03-02 20:24:39 +0100

    Custom Hooks till React Router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />;
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"), //empty object for passing an id for example
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Doint",
-//       animal: "Cat",
-//       breed: "Mix",
-//     }),
-//   ]);
-// };
 
 render(<App />, document.getElementById("root"));
